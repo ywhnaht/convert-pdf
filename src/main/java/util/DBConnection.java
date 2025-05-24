d6df8bf5616522287a256d54a1def5c7e49fc538
Huy Thanh <hothanhhuy24062004@gmail.com>
2025-05-24 23:16:47 +0700

    change DBConnection

--- src/main/java/util/DBConnection.java
+++ src/main/java/util/DBConnection.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/dulieu?useUnicode=true&characterEncoding=UTF-8&serverTimezone=UTC";
-    private static final String USER = "root";
-    private static final String PASSWORD = "huyho2004";
+    private static final String URL = "jdbc:mysql://localhost:3307/ltmang?useUnicode=true&characterEncoding=UTF-8&serverTimezone=UTC";
+    private static final String USER = "user";
+    private static final String PASSWORD = "password";
 
     static {
         try {
